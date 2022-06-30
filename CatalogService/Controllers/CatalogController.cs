653b03862ffeb76d5459cd6959fd1dc543580976
rob.brz <robert.brzozowski@ftsgps.com>
2022-06-30 12:31:17 +0200

    microservices sample

---
+++ CatalogService/Controllers/CatalogController.cs
@@ -0,0 +1,27 @@
+﻿using CatalogService.Database;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CatalogController : ControllerBase
+    {
+        DatabaseContext _db;
+
+        public CatalogController(DatabaseContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> GetProductes()
+        {
+            return _db.Products.ToList();
+        }
+    }
+}
---
+++ CatalogService/Database/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogService.Database
+{
+    public class Product
+    {
+        public int ProductId{ get; set; }
+        public string Name{ get; set; }
+        public string Description { get; set; }
+    }
+}
